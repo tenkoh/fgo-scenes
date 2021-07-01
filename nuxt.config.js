d2c33e416893beeadc06ccb3f89b62d04089afb2
tenkoh <gardencr+github@gmail.com>
2021-07-01 10:36:25 +0900

    wip: add production build setup to reduce bundle size

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Fate/Grand Order 二部六章名場面「そういうところだぞ村正ぁーーーー！」のパロディ画像ジェネレータです。さぁ、皆さんご一緒に！ ＼村正ぁ！／' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
@@ -40,6 +40,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    'nuxt-purgecss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -47,6 +48,39 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
+
+  purgeCSS: {
+    enabled: process.env.NODE_ENV === 'production',
+    paths: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      './node_modules/vuetify/dist/vuetify.js',
+      'assets/**/*.scss',
+    ],
+    styleExtensions: ['.css'],
+    whitelist: ['v-application', 'v-application--wrap', 'layout', 'row', 'col'],
+    whitelistPatterns: [
+      /^v-((?!application).)*$/,
+      /^theme--*/,
+      /.*-transition/,
+      /^justify-*/,
+      /^p*-[0-9]/,
+      /^m*-[0-9]/,
+      /^text--*/,
+      /--text$/,
+      /^row-*/,
+      /^col-*/,
+    ],
+    whitelistPatternsChildren: [/^v-((?!application).)*$/, /^theme--*/],
+    extractors: [
+      {
+        extractor: (content) => content.match(/[A-z0-9-:\\/]+/g) || [],
+        extensions: ['html', 'vue', 'js'],
+      },
+    ],
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
@@ -56,11 +90,17 @@ export default {
     defaultAssets: false,
     customVariables: ['~/assets/variables.scss'],
     optionsPath: './vuetify.options.js',
-    // treeShake: true,
+    treeShake: true,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extractCSS: { ignoreOrder: true },
+    optimization: {
+      splitChunks: {
+        maxSize: 128000,
+      },
+    },
   },
 
   generate: {
